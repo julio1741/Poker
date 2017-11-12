8e6c175eca45cb1933a3813873357eade85df452
julio@acid.cl <julio@acid.cl>
2017-11-11 21:10:27 -0300

    Just 2 hands

--- app/controllers/deck_controller.rb
+++ app/controllers/deck_controller.rb
@@ -2,8 +2,7 @@ class DeckController < ApplicationController
   def index
   	dealer = Dealer.new
   	@token = dealer.get_token
-  	@flop = dealer.get_cards(@token, 5)
-  	@hand_one = dealer.get_cards(@token, 2)
-  	@hand_two = dealer.get_cards(@token, 2)
+  	@hand_one = dealer.get_cards(@token, 5)
+  	@hand_two = dealer.get_cards(@token, 5)
   end
 end
