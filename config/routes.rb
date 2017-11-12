26061acc050a5a8cbcfa468bee12b146c8f5b9ac
julio@acid.cl <julio@acid.cl>
2017-11-12 19:40:12 -0300

    Change root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+
+  root :to => 'deck#index'
   get 'deck/index'
 
-  get 'deck' => 'deck#index'
   get 'new_hands/:token' => 'deck#new_hands', :as => :new_hands
 end
