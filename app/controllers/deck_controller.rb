4ecb8d30c336a21e6a87fa9ac4c4fb28f97ebdc9
julio@acid.cl <julio@acid.cl>
2017-11-12 23:03:01 -0300

    Add session expire condition

--- app/controllers/deck_controller.rb
+++ app/controllers/deck_controller.rb
@@ -4,7 +4,7 @@ class DeckController < ApplicationController
     @token = dealer.get_token
     @hand_one = dealer.get_cards(@token, 5)
     @hand_two = dealer.get_cards(@token, 5)
-    @out_of_hands if [404,405].include? @hand_one or [404,405].include? @hand_two
+    @out_of_hands = "Session Deck Expired!" if  @hand_one == 404 or @hand_two == 404
     unless @out_of_hands
       winner = dealer.winner_hand(@hand_one, @hand_two)
       @winner_hand = winner.first
@@ -17,7 +17,8 @@ class DeckController < ApplicationController
     @token = params[:token]
     @hand_one = dealer.get_cards(@token, 5)
     @hand_two = dealer.get_cards(@token, 5)
-    @out_of_hands = "Out of Hands!" if [404,405].include? @hand_one or [404,405].include? @hand_two
+    @out_of_hands = "Session Deck Expired!" if  @hand_one == 404 or @hand_two == 404
+    @out_of_hands = "Out of Hands!" if  @hand_one == 405 or @hand_two == 405
     unless @out_of_hands
       winner = dealer.winner_hand(@hand_one, @hand_two)
       @winner_hand = winner.first
